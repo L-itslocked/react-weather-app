b2c3a8ab5056f5b5635ab9b1b5cc6a973ef8d6d1
L-ItsLocked <L-itslocked@users.noreply.github.com>
2022-12-07 13:09:57 -0500

    Create Footer.js and add content

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/L-itslocked/Halloween-weather-app"
+        id="open-source-link"
+      >
+        Open-source code
+      </a>{" "}
+      by: Laura Doyon
+    </footer>
+  );
+}
